ec9c67d0b7f0e76885e5fca9990f097d3b29f682
Felipe Guerra <97766035+0Bellum0@users.noreply.github.com>
2023-03-07 22:02:00 -0300

    Added de creation of the DB.

--- dump-aluno-202303072143.sql
+++ dump-aluno-202303072143.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `Department_Info`
 --
 
+CREATE DATABASE IF NOT EXISTS aluno;
+
 DROP TABLE IF EXISTS `Department_Info`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
