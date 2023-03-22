5e7daf0eb30ef32fa044ebb2e23311470d964df5
0bellum0 <fpguerra00@gmail.com>
2023-03-21 21:07:10 -0300

    starting php

--- conexao.php
+++ conexao.php
@@ -1,3 +1,18 @@
 <?php
 
 echo "ola mundo <br>";
+
+
+
+$salario = 100;
+
+echo 'Salario: ' . $salario; //versao 7.4-
+
+echo "Salario:  {$salario}"; //versao 7.4+
+
+//. --> concatenar
+//{} --> concatenar
+//texto{$var} --> necessario ""
+
+
+
