90e62c024fc91096ce16aefa4c8cd41cb624297b
0bellum0 <fpguerr00@gmail.com>
2023-03-28 21:09:32 -0300

    feito o exercicio

--- exercicios_em_aula/separando_pares_e_impares.php
+++ exercicios_em_aula/separando_pares_e_impares.php
@@ -7,13 +7,16 @@ $vet_impar = [];
 while($numero<=300000){
     if($numero%2==0){
         $vet_par = array_push([$numero]);
-        //$vet_par[] = [$numero];
+        /*$vet_par[] = [$numero]; mesma coisa*/
     }
     else{
         $vet_impar = array_push([$numero]);
-        //$vet_impar[] = [$numero];
+        /*$vet_impar[] = [$numero]; mesma coisa*/
     }
     $numero++;
 }
+
+echo "Pares: {$vet_par}<br>";
+echo "impares {$vet_impares}<br>"
 
 ?>
