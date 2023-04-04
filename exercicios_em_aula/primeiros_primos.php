11a37a55e60956028b6286fe86ce40aad0187e97
0bellum0 <fpguerra00@gmail.com>
2023-04-04 20:59:17 -0300

    Feito o exercicio para achar os primos de 0 a 100

---
+++ exercicios_em_aula/primeiros_primos.php
@@ -0,0 +1,20 @@
+<?php
+
+$primos = [];
+
+for($numero=2; $numero<=100; $numero++){
+    for($i=1, $cont=0; $i<=$numero; $i++){
+        if($numero%$i==0){
+            $cont++;
+        }
+    }
+    if($cont==2){
+        array_push($primos, $numero);
+    }
+}
+
+echo "Primos: " . implode("," ,$primos);
+
+//Vai gerar todos os primos de 0 a 100 || Funciona :)
+
+?>
