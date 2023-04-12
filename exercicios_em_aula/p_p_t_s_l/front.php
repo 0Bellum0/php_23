498a3c70488eff0ab4700f008a4e8e628f3cb1bf
Felipe Guerra <97766035+0Bellum0@users.noreply.github.com>
2023-04-12 11:31:27 -0300

    update front.php

--- exercicios_em_aula/p_p_t_s_l/front.php
+++ exercicios_em_aula/p_p_t_s_l/front.php
@@ -5,10 +5,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pedra, Papel ou Tesoura</title>
+    <title>Pedra, Papel, Tesoura, Spock ou Lagarto</title>
 </head>
 
-<form name="form" method="get" action="pedra_papel_tesoura.php">
+<form name="form" method="get" action="p_p_t_s_l.php">
     <input id="pap" type="radio" value="0" name="escolha">
     <label for="pap">Papel</label>
 <Br>
@@ -17,6 +17,12 @@
 <Br>
     <input id="tes" type="radio" value="2" name="escolha">
     <label for="tes">Tesoura</label>
+<br>
+    <input id="spk" type="radio" value="3" name="escolha">
+    <label for="spk">Spock</label>
+<br>
+    <input id="lag" type="radio" value="4" name="escolha">
+    <label for="lag">Lagarto</label>
 
 <br>
 <hr>
@@ -25,4 +31,4 @@
 
 </form>
 
-</html
+</html>
