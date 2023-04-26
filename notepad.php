ac56e0cc524c7cb5f6886d04e83a83b3c45553a8
0bellum0 <fpguerra00@gmail.com>
2023-04-25 21:59:29 -0300

    introdução a funções

--- notepad.php
+++ notepad.php
@@ -7,13 +7,13 @@ $var = ? --> $ variavel
 
 $var = "" ou '' --> para strings
 
-%2 --> resto por 2 || 2 podendo ser qualquer numero || igual em C
+%2 --> resto por 2 | 2 podendo ser qualquer numero | igual em C
 
 % --> operador mod
 
-$vet = []; --> declaração de vetor || vetor em php é dinâmico :)
+$vet = []; --> declaração de vetor | vetor em php é dinâmico :)
 
-[] --> vetor || array
+[] --> vetor | array
 
 
 [
@@ -25,11 +25,11 @@ $vet = []; --> declaração de vetor || vetor em php é dinâmico :)
 
 $vet_par[] = [$numero];
 
-array_push(); --> função nativa para atribuir os valores para dentro do vetor || não pode ser passado por referência
+array_push(); --> função nativa para atribuir os valores para dentro do vetor | não pode ser passado por referência
 
 $var1 .= $var2 --> concatena os valores denrto das variáveis
 
-+= || -= || == --> igual C esse tipo de coisa
++= | -= | == --> igual C esse tipo de coisa
 
 ++$var --> pré incremento
 
@@ -39,10 +39,20 @@ implode(); -->
 
 explode(); --> 
 
-<br> --> So no navegador || o do proprio php é: \n --> igual do C
+<br> --> So no navegador | o do proprio php é: \n --> igual do C
 
-Datetime();
-*/
+Datetime(); --> pega o horário fornecido 
+
+
+                           />    /> Passagem por parametro | pode ser diferente da variavel que esta sendo passada para ela
+function nome_da_função ($var, $var, ...){ --> declaração de funçoes em php
+
+    return ; --> caso a função retorne algo
+}
+
+nome_da_função($var); --> chamando a função criada | pode estar em qualquer posição do código
+                \>variavel global ou de fora do escopo da função que sera recebida pelo parametro da função
+*/ 
 
 /**
  * 
