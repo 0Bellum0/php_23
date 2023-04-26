b4ee49c7d8b7194540a33eaa109e62ab2281295c
0bellum0 <fpguerra00@gmail.com>
2023-04-25 21:04:24 -0300

    Terminado o ex de contar o tempo de jogo.

--- exercicios_em_aula/jogo_de_futebol.php
+++ exercicios_em_aula/jogo_de_futebol.php
@@ -1,24 +1,24 @@
 <?php
 
-$inicio = '12:30:00';
-$fim = '13:50:00';
+$inicio = '23:30:00';
+$fim = '01:10:27';
 
 /*$diferenca = Datetime(fim) - Datetime(inicio); --> faz a mesma coisa que o codigo abaixo*/
 
 $arr_ini = explode(":", $inicio);
 $arr_fim = explode(":", $fim);
 
-$h_ini = $arr_ini[0];
-$min_ini = $arr_ini[1];
-$seg_ini = $arr_ini[2];
-
-$h_fim = $arr_fim[0];
-$min_fim = $arr_fim[1];
-$seg_fim = $arr_fim[2];
+//23:59:59 --> 86399
+//24:00:00 --> 86400
+//00:00:00 --> 0
 
 $inicio = ($arr_ini[0]*3600) + ($arr_ini[1]*60) + $arr_ini[2];
 
 $fim = ($arr_fim[0]*3600) + ($arr_fim[1]*60) + $arr_fim[2];
+
+if($inicio > $fim){
+    $fim += 86400;
+}
 
 $tempo = $fim - $inicio;
 
@@ -29,4 +29,6 @@ $min = $tempo/60;
 $tempo = $tempo%60;
 
 $seg = $tempo;
+
+echo "Tempo decorrido: " . intval($hora) . " hora, " . intval($min) . " minutos e {$seg} segundos";
 ?>
